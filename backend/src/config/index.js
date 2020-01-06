71357db4c23b5edeb450ca688e7c4121211184d7
brandtnick <niiick@live.se>
2020-01-06 21:47:00 +0100

    mm

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -1,8 +1,12 @@
 'use strict';
+
+const username = 'admin';
+const password = 'gT1xXad221x';
+
 const config = {
   PORT: process.env.PORT || 8080,
   mongo: {
-    URI: 'mongodb://localhost/time-reports',
+    URI: 'mongodb://' + username + ':' + password + '@mongodb:27017/timereports?authSource=admin',
     options: { useNewUrlParser: true },
   },
 };
